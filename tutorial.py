8d1e7c3d2f08d768bdabc78408b2c0c18d078ff1
TTCooper-PhD <tcoope2@gmail.com>
2023-04-02 11:47:39 -0400

    FASTA Load

--- tutorial.py
+++ tutorial.py
@@ -23,3 +23,29 @@ print(len(total_proteins))
 diann_path="C:\\DIA-NN\\1.8.1\\diaNN.exe"
 target_files = [os.path.join(f"{diann_input_folder}", f) for f in os.listdir(f"{diann_input_folder}") if f.lower().endswith(".tsv")]
 report_files = run_dia_nn(target_files, mzml_folder, "FirstBake_Forward", diann_path, 1.0)
+
+
+cmd = f"{diann_path} \
+--f \"Target_Folder\File1_-35.mzML\" \
+--lib \"\" \
+--threads 30 \
+--verbose 3 \
+--out \"report.tsv\" \
+--qvalue 0.01 \
+--matrices \
+--out-lib \"report-lib.tsv\" \
+--gen-spec-lib \
+--predictor \
+--fasta \"FASTA_HUMAN_Apr2023.fasta\" \
+--fasta-search \
+--min-fr-mz 200 \
+--max-fr-mz 1800 \
+--met-excision --cut K*,R* \
+--missed-cleavages 1 \
+--min-pep-len 7 --max-pep-len 30 \
+--min-pr-mz 300 --max-pr-mz 1800 \
+--min-pr-charge 1 --max-pr-charge 4 \
+--unimod4 --var-mods 1 --var-mod UniMod:35,15.994915,M --var-mod UniMod:1,42.010565,*n --monitor-mod UniMod:1"
+
+# Execute the command
+subprocess.run(cmd, shell=True, check=True)
